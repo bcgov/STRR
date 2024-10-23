667c17f6a1d445a2f8e5064c8fd21fbddb57a2d3
Avni Salhotra <avni.salhotra@gov.bc.ca>
2024-10-23 09:38:21 -0700

    styling

--- strr-web/components/bcros/form-section/property/Details.vue
+++ strr-web/components/bcros/form-section/property/Details.vue
@@ -24,7 +24,6 @@
         </UFormGroup>
       </div>
 
-      <!-- Business License Field -->
       <div class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="businessLicense" class="d:pr-[16px] flex-grow">
           <UInput
@@ -38,7 +37,6 @@
         </UFormGroup>
       </div>
 
-      <!-- Business License Expiry Date Field -->
       <div v-if="businessLicense" class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="businessLicenseExpiryDate" class="d:pr-[16px] flex-grow">
           <UInput
@@ -57,7 +55,6 @@
         </UFormGroup>
       </div>
 
-      <!-- Property Type Dropdown -->
       <div class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="propertyType" class="d:pr-[16px] flex-grow" :error="propertyTypeError">
           <USelect
@@ -66,14 +63,13 @@
             :placeholder="t('createAccount.propertyForm.propertyType')"
             :options="propertyTypes"
             option-attribute="name"
-            class="w-full"
+            style="color: #1a202c; /* text-gray-900 */ dark:text-white; /* Override with dark mode text color */"
             @blur="emit('validateProperty')"
             @change="emit('validateProperty')"
           />
         </UFormGroup>
       </div>
 
-      <!-- Ownership Type Dropdown -->
       <div class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="ownershipType" class="d:pr-[16px] flex-grow" :error="ownershipTypeError">
           <USelect
@@ -82,15 +78,14 @@
             :placeholder="t('createAccount.propertyForm.ownershipType')"
             :options="ownershipTypes"
             option-attribute="name"
-            class="w-full"
             :error="ownershipTypeError"
+            style="color: #1a202c; /* text-gray-900 */ dark:text-white; /* Override with dark mode text color */"
             @blur="emit('validateOwnership')"
             @change="emit('validateOwnership')"
           />
         </UFormGroup>
       </div>
 
-      <!-- Type of Space Dropdown -->
       <div class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="typeOfSpace" class="d:pr-[16px] flex-grow" :error="typeOfSpaceError">
           <USelect
@@ -99,13 +94,12 @@
             :placeholder="t('createAccount.propertyForm.typeOfSpace')"
             :options="typeOfSpaceOptions"
             option-attribute="name"
-            class="w-full"
+            style="color: #1a202c; /* text-gray-900 */ dark:text-white; /* Override with dark mode text color */"
             @blur="emit('validateTypeOfSpace')"
             @change="emit('validateTypeOfSpace')"
           />
         </UFormGroup>
       </div>
-      <!-- Is Rental Unit on the Same Property as Principal Residence? Dropdown -->
       <div class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="isOnSameProperty" class="d:pr-[16px] flex-grow">
           <USelect
@@ -114,13 +108,12 @@
             :placeholder="t('createAccount.propertyForm.isOnSameProperty')"
             :options="isOnSamePropertyOptions"
             option-attribute="name"
-            class="w-full"
+            style="color: #1a202c; /* text-gray-900 */ dark:text-white; /* Override with dark mode text color */"
             @change="handleIsOnSamePropertyChange"
           />
         </UFormGroup>
       </div>
 
-      <!-- Number of Rooms for Rent (conditionally displayed) -->
       <div v-if="isOnSameProperty === 'Yes'" class="flex flex-row justify-between w-full mb-[40px] mobile:mb-[16px]">
         <UFormGroup name="numberOfRooms" class="d:pr-[16px] flex-grow">
           <div class="flex items-center">
@@ -151,7 +144,6 @@
 
 const { t } = useTranslation()
 
-// Define models for each form field
 const propertyType = defineModel<string>('propertyType')
 const ownershipType = defineModel<string>('ownershipType')
 const businessLicense = defineModel<string>('businessLicense')
@@ -161,14 +153,12 @@ const typeOfSpace = defineModel<string>('typeOfSpace')
 const isOnSameProperty = defineModel<string>('isOnSameProperty')
 const numberOfRooms = ref<number>(1)
 
-// Watch for changes in businessLicense to clear expiry date if necessary
 watch(businessLicense, (): void => {
   if (!businessLicense.value) {
     formState.propertyDetails.businessLicenseExpiryDate = ''
   }
 })
 
-// Function to handle dropdown change for "isOnSameProperty"
 const handleIsOnSamePropertyChange = () => {
   if (isOnSameProperty.value === 'No') {
     numberOfRooms.value = 0
@@ -177,7 +167,6 @@ const handleIsOnSamePropertyChange = () => {
   }
 }
 
-// Increment and decrement room count
 const incrementRooms = () => {
   numberOfRooms.value++
 }
@@ -188,7 +177,6 @@ const decrementRooms = () => {
   }
 }
 
-// Emit events for validation
 const emit = defineEmits([
   'validateOwnership',
   'validateProperty',
