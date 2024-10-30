33e2378cabbb3c43a429f64b194c52e6b9b06ff5
Avni Salhotra <avni.salhotra@gov.bc.ca>
2024-10-30 13:20:15 -0700

    update property type interface

--- strr-web/interfaces/property-type-map-i.ts
+++ strr-web/interfaces/property-type-map-i.ts
@@ -1,3 +1,14 @@
 export type PropertyTypeMapI = {
-  [key in 'PRIMARY' | 'SECONDARY' | 'ACCESSORY' | 'FLOAT_HOME' | 'OTHER']: string;
+  [key in
+    | 'SINGLE_FAMILY_HOME'
+    | 'SECONDARY_SUITE'
+    | 'ACCESSORY_DWELLING'
+    | 'TOWN_HOME'
+    | 'MULTI_UNIT_HOUSING'
+    | 'CONDO_APARTMENT'
+    | 'RECREATIONAL_PROPERTY'
+    | 'BED_AND_BREAKFAST'
+    | 'STRATA_HOTEL'
+    | 'FLOAT_HOME'
+  ]: string;
 };
