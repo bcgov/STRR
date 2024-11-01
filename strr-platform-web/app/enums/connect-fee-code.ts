70543130b6f50c1375473c974e077b260dddad41
Kial Jinnah <kialj876@gmail.com>
2024-11-01 13:22:57 -0400

    revert file name change for git diff
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- strr-platform-web/app/enums/connect-fee-code.ts
+++ strr-platform-web/app/enums/connect-fee-code.ts
@@ -1,4 +1,4 @@
-// TODO move to strr-base-web layer (connect layer will not contain enums for fee code)
+// TODO: rename file / move to strr-base-web layer (connect layer will not contain enums for fee code)
 export enum StrrFeeCode {
   STR_HOST = 'RENTAL_FEE',
   STR_PLAT_LG = 'PLATREG_LG',
--- strr-platform-web/app/enums/str-fee-entity-type.ts
+++
@@ -1,4 +0,0 @@
-// TODO move to strr-base-web layer (connect layer will not contain enums for fee entity type)
-export enum StrrFeeEntityType {
-  STRR = 'STRR'
-}
