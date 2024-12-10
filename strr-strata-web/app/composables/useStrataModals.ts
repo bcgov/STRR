80eb5f1440296fa7452c286fd4adc4c0f2b30db0
deetz99 <dietrich@wolpert.ca>
2024-12-09 16:00:32 -0800

    update error modal text

--- strr-strata-web/app/composables/useStrataModals.ts
+++ strr-strata-web/app/composables/useStrataModals.ts
@@ -1,20 +1,27 @@
 // https://ui.nuxt.com/components/modal#control-programmatically
 
 // currently no unique strata modals
-// import {
+import {
 //   ModalBase,
-//   ModalHelpRegisterStrataHotel
-// } from '#components'
+  ModalErrorApplicationSubmit
+} from '#components'
 
 export const useStrataModals = () => {
   const modal = useModal()
   // const { t } = useI18n()
+
+  function openApplicationSubmitErrorModal (e: unknown) {
+    modal.open(ModalErrorApplicationSubmit, {
+      error: e
+    })
+  }
 
   function close () {
     modal.close()
   }
 
   return {
+    openApplicationSubmitErrorModal,
     close
   }
 }
