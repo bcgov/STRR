662d22faa96fdf31c01c2176ae0340da1c9f4c32
deetz99 <dietrich@wolpert.ca>
2024-12-11 14:20:58 -0800

    change

--- strr-platform-web/app/middleware/application-page.ts
+++ strr-platform-web/app/middleware/application-page.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   // TODO: update with unique code check for renewals?
   if (to.query.override !== 'true') {
-    const localePath = useLocalePath()
     const { application, loadPermitData } = useStrrBasePermit()
     await loadPermitData(undefined, ApplicationType.PLATFORM)
 
     if (application.value !== undefined) {
+      const localePath = useLocalePath()
       return navigateTo(localePath('/platform/dashboard'))
     }
   }
