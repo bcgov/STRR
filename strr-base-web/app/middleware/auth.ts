3d3bbc849f71ae184ad7d868c0f576556a7792ff
deetz99 <dietrich@wolpert.ca>
2024-12-11 15:38:24 -0800

    middleware update

--- strr-base-web/app/middleware/auth.ts
+++ strr-base-web/app/middleware/auth.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware(() => {
   if (!isAuthenticated.value) { // redirect to login page if user not authenticated
     const localePath = useLocalePath()
     return navigateTo(localePath('/auth/login'))
-  } else if (!allowedIdps.includes(kcUser.value.loginSource.toLowerCase())) { // log user out and redirect to login page if user authenticated with invalid login source
+  } else if (isAuthenticated.value && !allowedIdps.includes(kcUser.value.loginSource.toLowerCase())) { // log user out and redirect to login page if user authenticated with invalid login source
     const locale = useNuxtApp().$i18n.locale.value
     const redirectUrl =
       useRuntimeConfig().public.baseUrl + locale + '/auth/login?invalidIdp=' + kcUser.value.loginSource
