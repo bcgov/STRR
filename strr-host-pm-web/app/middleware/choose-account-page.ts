7365dc38168394828f25f3ae8f0a3193dd2a151f
deetz99 <dietrich@wolpert.ca>
2024-12-12 11:58:58 -0800

    middleware update

--- strr-host-pm-web/app/middleware/choose-account-page.ts
+++ strr-host-pm-web/app/middleware/choose-account-page.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware(() => {
   const localePath = useLocalePath()
   const { kcUser } = useKeycloak()
 
-  if (accountStore.userAccounts.length === 0 && kcUser.value.loginSource !== LoginSource.BCEID) {
+  if (accountStore.userAccounts.length === 0 && kcUser.value.loginSource === LoginSource.BCSC) {
     return navigateTo({ path: localePath('/auth/account/create-new') }) // TODO: add return url param for redirect?
   }
 })
