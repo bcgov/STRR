65eb29c0209706cd3b1419bca116238ab74afe81
deetz99 <dietrich@wolpert.ca>
2025-01-02 16:15:36 -0800

    bump version

--- strr-host-pm-web/nuxt.config.ts
+++ strr-host-pm-web/nuxt.config.ts
@@ -39,8 +39,8 @@ export default defineNuxtConfig({
   },
 
   extends: [
-    // ['github:bcgov/STRR/strr-base-web', { install: true }],
-    '../strr-base-web', // dev only
+    ['github:bcgov/STRR/strr-base-web', { install: true }],
+    // '../strr-base-web', // dev only
     '@daxiom/nuxt-core-layer-test',
   ],
 
