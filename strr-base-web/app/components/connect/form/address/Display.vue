849df1ff805f304b130e153554ddd0b39b0e5283
deetz99 <dietrich@wolpert.ca>
2025-01-09 12:26:57 -0800

    fix address display util

--- strr-base-web/app/components/connect/form/address/Display.vue
+++ strr-base-web/app/components/connect/form/address/Display.vue
@@ -4,7 +4,22 @@ const props = defineProps<{
   useLocationDescLabel?: boolean
 }>()
 
-const addressData = computed(() => getAddressDisplayParts(props.address))
+const regionNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'region' })
+
+const addressData = computed(() => {
+  return [
+    `${props.address.street},` || [
+      [props.address.unitNumber, props.address.streetNumber].filter(val => !!val).join('-'),
+      `${props.address.streetName},`
+    ].filter(val => !!val).join(' ') || '',
+    props.address.streetAdditional ? `${props.address.streetAdditional},` : '',
+    [
+      `${props.address.city},`,
+      `${props.address.region}\u00A0`, props.address.postalCode
+    ].filter(val => !!val).join(' ') || '',
+    props.address.country ? (regionNamesInEnglish.of(props.address.country) || props.address.country) : ''
+  ].filter(val => !!val)
+})
 </script>
 <template>
   <div data-testid="address-display">
