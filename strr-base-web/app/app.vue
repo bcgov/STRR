17e50de6291a91ce6607033f2ae6cdc2d25f8fa0
deetz99 <dietrich@wolpert.ca>
2025-01-15 13:27:39 -0800

    update apps

--- strr-base-web/app/app.vue
+++ strr-base-web/app/app.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// import { initWebMsg } from './utils/initWebMsg'
-
 const i18nHead = useLocaleHead({
   addDirAttribute: true,
   addSeoAttributes: true
@@ -15,18 +13,21 @@ useHead({
 })
 
 onMounted(async () => {
-  const { ldClient, getStoredFlag } = useConnectLaunchdarklyStore()
-  await ldClient?.waitUntilReady()
-  const enableSbcWebMsg = getStoredFlag('enable-sbc-web-messenger')
   const msgConfig = useAppConfig().strrBaseLayer.sbcWebMsg
 
-  if (ldClient && enableSbcWebMsg && msgConfig.enable) {
-    const rtc = useRuntimeConfig().public
-    const genesysUrl = rtc.genesysUrl as string
-    const environmentKey = rtc.genesysEnvironmentKey as string
-    const deploymentKey = rtc.genesysDeploymentKey as string
+  if (msgConfig.enable) {
+    const { ldClient, getStoredFlag } = useConnectLaunchdarklyStore()
+    await ldClient?.waitUntilReady()
+    const enableSbcWebMsg = getStoredFlag('enable-sbc-web-messenger')
 
-    initWebMsg(genesysUrl, environmentKey, deploymentKey)
+    if (ldClient && enableSbcWebMsg) {
+      const rtc = useRuntimeConfig().public
+      const genesysUrl = rtc.genesysUrl as string
+      const environmentKey = rtc.genesysEnvironmentKey as string
+      const deploymentKey = rtc.genesysDeploymentKey as string
+
+      initWebMsg(genesysUrl, environmentKey, deploymentKey)
+    }
   }
 })
 </script>
