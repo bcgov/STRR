1288bfeba2e3c2d94514a9b7a3dc3f61688b9763
deetz99 <dietrich@wolpert.ca>
2025-01-29 13:34:23 -0800

    add strata category to examiners review page

---
+++ strr-base-web/app/enums/strata-hotel-category.ts
@@ -0,0 +1,6 @@
+// will be used in strata, host and examiner apps
+export enum StrataHotelCategory {
+  FULL_SERVICE = 'FULL_SERVICE',
+  MULTI_UNIT_NON_PR = 'MULTI_UNIT_NON_PR',
+  POST_DECEMBER_2023 = 'POST_DECEMBER_2023'
+}
--- strr-examiner-web/nuxt.config.ts
+++ strr-examiner-web/nuxt.config.ts
@@ -39,8 +39,8 @@ export default defineNuxtConfig({
   },
 
   extends: [
-    ['github:bcgov/STRR/strr-base-web', { install: true }]
-    // '../strr-base-web' // dev only
+    // ['github:bcgov/STRR/strr-base-web', { install: true }]
+    '../strr-base-web' // dev only
   ],
 
   imports: {
