80e172c9893607a71129024eaf3eedf2ad6ae83a
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-03-26 14:35:29 -0700

    26229 UI - UXA Update
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-examiner-web/app/components/Table/Header/Select.vue
+++ strr-examiner-web/app/components/Table/Header/Select.vue
@@ -80,14 +80,6 @@ onMounted(() => {
         multiple
         :searchable
         clear-search-on-close
-        :ui="{
-          popper: {
-            strategy: 'fixed',
-            placement: 'bottom-start',
-            offset: 4
-          },
-          dropdown: `min-w-[${initialWidth}] z-[100] overflow-visible`
-        }"
       >
         <template #default="{ open }">
           <!-- TODO: aria labels? -->
@@ -132,7 +124,7 @@ onMounted(() => {
           </div>
           <div
             v-else
-            class="flex cursor-pointer items-center gap-2 px-4 py-2"
+            class="flex cursor-pointer items-center gap-2 px-2 py-1"
           >
             <UCheckbox
               :model-value="selected"
