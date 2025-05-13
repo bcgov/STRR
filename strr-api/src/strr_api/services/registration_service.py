1de10f043ad6dd3d4472ef1427a7ef638efe077f
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-05-13 08:23:02 -0700

    27289 - decline associated provisional review applications
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-api/src/strr_api/services/registration_service.py
+++ strr-api/src/strr_api/services/registration_service.py
@@ -45,6 +45,7 @@ from weasyprint import HTML
 from strr_api.enums.enum import RegistrationSortBy, RegistrationStatus, RegistrationType
 from strr_api.models import (
     Address,
+    Application,
     Certificate,
     Contact,
     Document,
@@ -565,6 +566,24 @@ class RegistrationService:
         }
         registration.status = status
         registration.save()
+
+        if status == RegistrationStatus.CANCELLED.value:
+            applications_to_decline = Application.query.filter(
+                Application.registration_id == registration.id,
+                Application.status == Application.Status.PROVISIONAL_REVIEW.value
+            ).all()
+
+            for application in applications_to_decline:
+                application.status = Application.Status.DECLINED.value
+                application.save()
+                EventsService.save_event(
+                    event_type=Events.EventType.APPLICATION, 
+                    event_name=Events.EventName.MANUALLY_DENIED, 
+                    application_id=application.id,
+                    user_id=reviewer.id if reviewer else None,
+                    visible_to_applicant=True,
+                )
+
         reviewer_id = reviewer.id if reviewer else None
         EventsService.save_event(
             event_type=Events.EventType.REGISTRATION,
