6f5e817197bfb7b92e658461ad6b20475c46f353
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-05-21 12:31:25 -0700

    27122 fix
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-api/src/strr_api/responses/RegistrationSerializer.py
+++ strr-api/src/strr_api/responses/RegistrationSerializer.py
@@ -93,7 +93,7 @@ class RegistrationSerializer:
             registration_data["header"]["applicationNumber"] = application.application_number
             registration_data["header"]["applicationDateTime"] = application.application_date.isoformat()
             registration_data["header"]["organizationName"] = (
-                application.application_json.get("registration").get("strRequirements").get("organizationNm")
+                application.application_json.get("registration").get("strRequirements", {}).get("organizationNm")
             )
             registration_data["header"]["reviewer"] = {}
             if application.reviewer_id:
