ac7c085509391d9d048fc94e410f63ec175fba32
Dima K <dima1086@hotmail.com>
2025-09-04 10:01:56 -0700

    UXA feedback updates for Decision Email

--- strr-examiner-web/app/components/DecisionPanel.vue
+++ strr-examiner-web/app/components/DecisionPanel.vue
@@ -57,7 +57,7 @@ const decisionButtons = [
     action: ApplicationActionsE.APPROVE,
     label: t('btn.approve'),
     color: 'green',
-    activeStyle: 'bg-str-bgGreen text-str-textGray hover:bg-str-bgGreen',
+    activeStyle: 'bg-str-bgGreen text-str-textGray hover:bg-str-bgGray',
     icon: 'i-mdi-check',
     disabled: !enableApproveButton.value
   },
@@ -65,7 +65,7 @@ const decisionButtons = [
     action: ApplicationActionsE.SEND_NOC,
     label: t('btn.sendNotice'),
     color: 'blue',
-    activeStyle: 'bg-str-bgBlue text-str-textGray hover:bg-str-bgBlue',
+    activeStyle: 'bg-str-bgBlue text-str-textGray hover:bg-str-bgGray',
     icon: 'i-mdi-send',
     disabled: !activeHeader.value.examinerActions.includes(ApplicationActionsE.SEND_NOC)
   },
@@ -73,7 +73,7 @@ const decisionButtons = [
     action: ApplicationActionsE.REJECT,
     label: t('btn.decline'),
     color: 'red',
-    activeStyle: 'bg-str-bgRed text-str-textGray hover:bg-str-bgRed',
+    activeStyle: 'bg-str-bgRed text-str-textGray hover:bg-str-bgGray',
     icon: 'i-mdi-close',
     disabled: !activeHeader.value.examinerActions.includes(ApplicationActionsE.REJECT),
     hidden: !isApplication.value
@@ -82,7 +82,7 @@ const decisionButtons = [
     action: RegistrationActionsE.CANCEL,
     label: t('btn.cancel'),
     color: 'red',
-    activeStyle: 'bg-str-bgRed text-str-textGray hover:bg-str-bgRed',
+    activeStyle: 'bg-str-bgRed text-str-textGray hover:bg-str-bgGray',
     icon: 'i-mdi-close',
     disabled: !activeHeader.value.examinerActions.includes(RegistrationActionsE.CANCEL),
     hidden: isApplication.value
@@ -262,7 +262,13 @@ onMounted(() => {
               :validate-on="['submit']"
               class="mt-4"
             >
-              <UFormGroup name="content">
+              <UFormGroup
+                name="content"
+                :ui="{
+                  wrapper: 'mb-4',
+                  error: 'text-xs/5 mt-1 absolute'
+                }"
+              >
                 <UTextarea
                   v-model="decisionEmailContent.content"
                   :placeholder="decisionEmailPlaceholder"
