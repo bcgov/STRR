26152cbcb1ecc59c153c317c92f642f44451a0eb
Kris <kris@daxiom.com>
2025-09-03 22:30:59 -0700

    Snapshot service

---
+++ strr-api/src/strr_api/services/snapshot_service.py
@@ -0,0 +1,56 @@
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the BSD 3 Clause License, (the "License");
+# you may not use this file except in compliance with the License.
+# The template for the license can be found here
+#    https://opensource.org/license/bsd-3-clause/
+#
+# Redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the
+# following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Snapshot service that helps take registration snapshots when required."""
+from datetime import datetime
+
+from strr_api.models import Registration, RegistrationSnapshot
+from strr_api.services.registration_service import RegistrationService
+
+
+class SnapshotService:
+    """Service to create registration snapshots"""
+
+    @staticmethod
+    def snapshot_registration(registration: Registration) -> RegistrationSnapshot:
+        """Creates registration snapshots."""
+        registration_snapshot = RegistrationSnapshot()
+        registration_snapshot.registration_id = registration.id
+        registration_snapshot.snapshot_datetime = datetime.utcnow()
+        registration_snapshot.snapshot_data = RegistrationService.serialize(registration=registration)
+        latest_snapshot = RegistrationSnapshot.find_latest_snapshot(registration.id)
+        registration_snapshot.version = (latest_snapshot.version + 1) if latest_snapshot else 1
+        registration_snapshot.save()
+        return registration_snapshot
