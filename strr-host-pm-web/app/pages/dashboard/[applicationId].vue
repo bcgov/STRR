9c5c3357cdc56bb1cb957a6979484ca97be7a69d
Dima K <dima1086@hotmail.com>
2025-10-08 16:10:47 -0700

    Display Renewal Draft ToDo

--- strr-host-pm-web/app/pages/dashboard/[applicationId].vue
+++ strr-host-pm-web/app/pages/dashboard/[applicationId].vue
@@ -23,6 +23,8 @@ const { unitAddress } = storeToRefs(useHostPropertyStore())
 
 const {
   isEligibleForRenewal,
+  hasRegistrationRenewalDraft,
+  renewalDraftId,
   renewalDueDate,
   renewalDateCounter,
   isRenewalPeriodClosed
@@ -146,7 +148,11 @@ definePageMeta({
 })
 
 // watch for Registration Renewals props and update related ToDos
-watch([isRenewalsEnabled, isRenewalPeriodClosed, registration, isEligibleForRenewal], () => {
+watch([isRenewalsEnabled,
+  isRenewalPeriodClosed,
+  registration,
+  isEligibleForRenewal,
+  hasRegistrationRenewalDraft], () => {
   const translationProps = {
     newLine: '<br/>',
     boldStart: '<strong>',
@@ -179,6 +185,22 @@ watch([isRenewalsEnabled, isRenewalPeriodClosed, registration, isEligibleForRene
           await navigateTo({
             path: localePath('/application'),
             query: { renew: 'true', registrationId: registration.value?.id }
+          })
+        }
+      }
+    })
+  } else if (isRenewalsEnabled && registration.value && hasRegistrationRenewalDraft.value) {
+    // todo for existing renewal draft
+    todos.value.push({
+      id: 'todo-renewal-draft',
+      title: 'Application to Renew Registration',
+      subtitle: 'DRAFT',
+      button: {
+        label: 'Resume',
+        action: async () => {
+          await navigateTo({
+            path: localePath('/application'),
+            query: { renew: 'true', applicationId: renewalDraftId.value }
           })
         }
       }
