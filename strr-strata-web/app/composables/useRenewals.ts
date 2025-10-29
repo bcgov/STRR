3cae9ce0f0602670c20119a38566a5c3b2a9b1b7
Dima K <dima1086@hotmail.com>
2025-10-29 13:16:26 -0700

    Prefill Strata renewal application

---
+++ strr-strata-web/app/composables/useRenewals.ts
@@ -0,0 +1,18 @@
+export const useRenewals = () => {
+  const isEligibleForRenewal = ref(false)
+
+  const { registration } = storeToRefs(useStrrStrataStore())
+
+  watch(registration, () => {
+    if (!registration.value) {
+      isEligibleForRenewal.value = false
+    }
+
+    // TODO: get renewals from Strata ToDo when ready, for now just check the status
+    isEligibleForRenewal.value = registration.value?.status === RegistrationStatus.EXPIRED
+  })
+
+  return {
+    isEligibleForRenewal
+  }
+}
