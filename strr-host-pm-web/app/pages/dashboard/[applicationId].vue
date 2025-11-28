ee6b9c9507d9f95fb4be2c2ba1d31cebe7d05b31
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-11-28 10:20:21 -0800

    31438 - revert legacy detail page changes & fix redirect middleware
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-host-pm-web/app/pages/dashboard/[applicationId].vue
+++ strr-host-pm-web/app/pages/dashboard/[applicationId].vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { watch } from 'vue'
+
 const { t } = useNuxtApp().$i18n
 const route = useRoute()
+const config = useRuntimeConfig().public
+const localePath = useLocalePath()
 const {
   loading,
   title,
@@ -11,22 +15,30 @@ const {
   application,
   registration,
   permitDetails,
+  renewalRegId,
   isPaidApplication,
   showPermitDetails,
   needsBusinessLicenseDocumentUpload
 } = storeToRefs(permitStore)
 const { unitAddress } = storeToRefs(useHostPropertyStore())
 
-const { owners, setupBreadcrumbs, setupOwners } = useDashboardPage()
-
 const {
-  todos,
-  addNocTodo,
-  addBusinessLicenseTodo,
-  setupRenewalTodosWatch
-} = useDashboardTodos()
+  isEligibleForRenewal,
+  hasRegistrationRenewalDraft,
+  hasRegistrationRenewalPaymentPending,
+  renewalDraftId,
+  renewalPaymentPendingId,
+  renewalDueDate,
+  renewalDateCounter,
+  isRenewalPeriodClosed,
+  getRegistrationRenewalTodos
+} = useRenewals()
 
-setupRenewalTodosWatch()
+const { getAccountApplication, deleteApplication } = useStrrApi()
+
+const todos = ref<Todo[]>([])
+const owners = ref<ConnectAccordionItem[]>([])
+const { isRenewalsEnabled, isNewDashboardEnabled } = useHostFeatureFlags()
 
 onMounted(async () => {
   loading.value = true
@@ -40,10 +52,50 @@ onMounted(async () => {
     ApplicationType.HOST
   ))
 
-  addNocTodo()
-  addBusinessLicenseTodo()
+  if (registration.value?.nocStatus === RegistrationNocStatus.NOC_PENDING) {
+    const translationProps = {
+      newLine: '<br/>',
+      boldStart: '<strong>',
+      boldEnd: '</strong>',
+      linkStart: "<button type='button'" +
+        "onClick=\"document.getElementById('summary-supporting-info').scrollIntoView({ behavior: 'smooth' })\"" +
+        "class='text-blue-500 underline'>",
+      linkEnd: '</button>'
+    }
+
+    const nocEndDate = dateToString(permitDetails.value.nocEndDate as Date)
+
+    todos.value.push({
+      id: 'todo-reg-noc-add-docs',
+      title: `${t('todos.registrationNoc.title1')} ${nocEndDate} ${t('todos.registrationNoc.title2')}`,
+      subtitle: `${t('todos.registrationNoc.general', translationProps)}`
+    })
+  }
+
+  // Add business license upload todo if conditions are met
+  if (needsBusinessLicenseDocumentUpload.value) {
+    const translationProps = {
+      newLine: '<br/>',
+      boldStart: '<strong>',
+      boldEnd: '</strong>',
+      linkStart: "<button type='button'" +
+        "onClick=\"document.getElementById('summary-supporting-info').scrollIntoView({ behavior: 'smooth' })\"" +
+        "class='text-blue-500 underline'>",
+      linkEnd: '</button>',
+      mailto: "<a href='mailto:STRregistry@gov.bc.ca' class='text-blue-500 underline'>STRregistry@gov.bc.ca</a>"
+    }
+
+    todos.value.push({
+      id: 'todo-business-license-upload',
+      title: t('todos.businessLicense.title'),
+      subtitle: t('todos.businessLicense.subtitle', translationProps),
+      icon: 'i-mdi-alert',
+      iconClass: 'text-orange-500'
+    })
+  }
 
   if (!permitDetails.value || !showPermitDetails.value) {
+    // TODO: probably not ever going to get here? Filing would launch from the other account dashboard?
     title.value = t('strr.title.dashboard')
   } else {
     // existing registration or application under the account
@@ -68,15 +120,28 @@ onMounted(async () => {
     setSideHeaderDetails(registration.value, application.value?.header)
 
     // set sidebar accordion reps
-    setupOwners()
+    owners.value = getHostPermitDashOwners()
 
-    // update breadcrumbs
-    setupBreadcrumbs()
+    // update breadcrumbs with strata business name
+    setBreadcrumbs([
+      {
+        label: t('label.bcregDash'),
+        to: config.registryHomeURL + 'dashboard',
+        appendAccountId: true,
+        external: true
+      },
+      {
+        label: t('strr.title.dashboard'),
+        to: localePath(isNewDashboardEnabled.value ? '/dashboard-new' : '/dashboard')
+      },
+      { label: permitDetails.value.unitAddress.nickname || t('strr.label.unnamed') }
+    ])
   }
 
   loading.value = false
 })
 
+// page stuff
 useHead({
   title: t('strr.title.dashboard')
 })
@@ -90,6 +155,120 @@ definePageMeta({
     return true
   }
 })
+
+// watch for Registration Renewals props and update related ToDos
+watch([isRenewalsEnabled,
+  isRenewalPeriodClosed,
+  registration,
+  isEligibleForRenewal,
+  hasRegistrationRenewalDraft,
+  hasRegistrationRenewalPaymentPending], () => {
+  const translationProps = {
+    newLine: '<br/>',
+    boldStart: '<strong>',
+    boldEnd: '</strong>'
+  }
+
+  if (isRenewalsEnabled && isRenewalPeriodClosed.value) {
+    // todo for renewal period closed after 3 years without renewal
+    todos.value.push({
+      id: 'todo-renew-registration-closed',
+      title: t('todos.renewalClosed.title'),
+      subtitle: t('todos.renewalClosed.subtitle', translationProps)
+    })
+  } else if (isRenewalsEnabled && registration.value && isEligibleForRenewal.value) {
+    const isOverdue = renewalDateCounter.value < 0
+    // label for the due days count
+    const dueDateCount = isOverdue
+      ? t('label.renewalOverdue')
+      : t('label.renewalDayCount', renewalDateCounter.value)
+
+    todos.value.push({
+      id: 'todo-renew-registration',
+      title: `${t('todos.renewal.title1')} ${renewalDueDate.value} ${t('todos.renewal.title2')} (${dueDateCount})`,
+      subtitle: t(isOverdue
+        ? 'todos.renewal.expired'
+        : 'todos.renewal.expiresSoon', translationProps),
+      buttons: [{
+        label: t('btn.renew'),
+        action: async () => {
+          renewalRegId.value = registration.value?.id
+          await navigateTo({
+            path: localePath('/application'),
+            query: { renew: 'true' }
+          })
+        }
+      }]
+    })
+  } else if (isRenewalsEnabled && registration.value && hasRegistrationRenewalDraft.value) {
+    // todo for existing renewal draft
+    todos.value.push({
+      id: 'todo-renewal-draft',
+      title: t('todos.renewalDraft.title'),
+      subtitle: t('todos.renewalDraft.subtitle'),
+      buttons: [
+        {
+          label: t('todos.renewalDraft.resumeButton'),
+          action: async () => {
+            renewalRegId.value = undefined // reset renewal id, so the draft app is loaded instead of registration
+            await navigateTo({
+              path: localePath('/application'),
+              query: { renew: 'true', applicationId: renewalDraftId.value }
+            })
+          }
+        },
+        {
+          label: t('todos.renewalDraft.deleteDraft'),
+          icon: 'i-mdi-delete',
+          action: async () => {
+            // delete draft application
+            await deleteApplication(renewalDraftId.value)
+            // remove renewal draft todo
+            todos.value = todos.value.filter(todo => todo.id !== 'todo-renewal-draft')
+            // reload registration renewal todos
+            await getRegistrationRenewalTodos()
+          }
+        }
+      ]
+    })
+  } else if (isRenewalsEnabled && registration.value && hasRegistrationRenewalPaymentPending.value) {
+    // todo for renewal payment pending
+    todos.value.push({
+      id: 'todo-renewal-payment-pending',
+      title: t('todos.renewalPayment.title'),
+      subtitle: t('todos.renewalPayment.subtitle'),
+      buttons: [{
+        label: t('todos.renewalPayment.button'),
+        action: async () => {
+          const { handlePaymentRedirect } = useConnectNav()
+          // Get the payment token
+          const applicationResponse = await getAccountApplication(
+            renewalPaymentPendingId.value
+          )
+          const paymentToken = applicationResponse?.header.paymentToken
+          const appNum = applicationResponse?.header.applicationNumber
+          if (paymentToken) {
+            handlePaymentRedirect(
+              paymentToken,
+              '/dashboard/' + appNum
+            )
+          }
+        }
+      }]
+    })
+  }
+}, { immediate: true })
+
+setBreadcrumbs([
+  {
+    label: t('label.bcregDash'),
+    to: config.registryHomeURL + 'dashboard',
+    appendAccountId: true,
+    external: true
+  },
+  { label: t('strr.title.dashboard'), to: localePath(isNewDashboardEnabled.value ? '/dashboard-new' : '/dashboard') },
+  { label: 'Item 1' }
+])
 </script>
 <template>
   <div
@@ -98,17 +277,91 @@ definePageMeta({
     class="flex flex-col gap-5 py-8 sm:flex-row sm:py-10"
   >
     <div class="flex-1 space-y-10">
-      <DashboardTodoSection :todos="todos" :loading="loading" />
-      <DashboardRentalSection :loading="loading" />
-      <DashboardSupportingInfoSection
+      <ConnectDashboardSection
+        id="to-do-section"
+        data-test-id="todo-section"
+        :title="$t('label.todo')"
+        :title-num="todos.length"
         :loading="loading"
-        :needs-business-license-document-upload="needsBusinessLicenseDocumentUpload"
-      />
+      >
+        <TodoEmpty v-if="!todos.length" data-test-id="todo-empty" />
+        <template v-else>
+          <template v-for="(todo, index) in todos" :key="todo.title">
+            <Todo
+              :id="todo.id"
+              :title="todo.title"
+              :subtitle="todo.subtitle"
+              :buttons="todo?.buttons"
+              :icon="todo?.icon"
+              :icon-class="todo?.iconClass"
+            />
+            <div v-if="index < todos.length - 1" class="h-px w-full border-b border-gray-100" />
+          </template>
+        </template>
+      </ConnectDashboardSection>
+      <ConnectDashboardSection
+        id="short-term-rental-section"
+        data-test-id="rental-section"
+        :title="$t('strr.label.shortTermRental')"
+        :loading="loading"
+      >
+        <SummaryProperty class="px-10 py-5" data-test-id="summary-property" />
+      </ConnectDashboardSection>
+      <ConnectDashboardSection
+        id="supporting-info-section"
+        data-test-id="supporting-info-section"
+        :title="$t('strr.label.supportingInfo')"
+        :loading="loading"
+      >
+        <UAlert
+          v-if="needsBusinessLicenseDocumentUpload"
+          color="yellow"
+          icon="i-mdi-alert"
+          :close-button="null"
+          variant="subtle"
+          :ui="{
+            inner: 'pt-0',
+            icon: {
+              base: 'flex-shrink-0 w-5 h-5 self-start'
+            }
+          }"
+        >
+          <template #title>
+            <span class="text-black">
+              <span class="font-bold">{{ t('alert.businessLicense.title') }}</span>
+              {{ t('alert.businessLicense.description') }}
+            </span>
+          </template>
+        </UAlert>
+        <SummarySupportingInfo
+          id="summary-supporting-info"
+          class="px-10 py-5"
+          data-test-id="summary-supporting-info"
+        />
+      </ConnectDashboardSection>
     </div>
-    <DashboardSidebar
-      :loading="loading"
-      :show-permit-details="showPermitDetails"
-      :owners="owners"
-    />
+    <div class="space-y-10 sm:w-[300px]">
+      <RegistrationTermsConditions
+        v-if="!loading"
+      />
+      <ConnectDashboardSection
+        id="individuals-business-section"
+        data-test-id="individuals-business-section"
+        :title="$t('strr.label.individualsBusinesses')"
+        :loading="loading"
+      >
+        <ConnectAccordion
+          v-if="showPermitDetails"
+          :items="owners"
+          multiple
+          data-test-id="owners-accordion"
+        />
+        <div v-else class="w-full bg-white p-5 opacity-50" data-test-id="complete-filing-message">
+          <p class="text-sm">
+            {{ $t('text.completeFilingToDisplay') }}
+          </p>
+        </div>
+      </ConnectDashboardSection>
+    </div>
   </div>
 </template>
