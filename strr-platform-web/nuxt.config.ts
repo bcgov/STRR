c381e8f9ff1e71b184f7c5d5cdc0636c4f87c5c0
Dima K <dima1086@hotmail.com>
2025-12-17 00:32:39 +0100

    Update Package Versions in Platform app (#961)
    * chore: update package versions for platform app
    
    * chore: update version of platform app itself
    
    * ci: skip sonarqube analysis of strr-web folder
    
    * refactor: remove fr-CA translation file from platform because same file exists in base later already

--- strr-platform-web/nuxt.config.ts
+++ strr-platform-web/nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   ],
 
   extends: [
-    ['github:bcgov/STRR/strr-base-web', { install: true }],
+    ['github:bcgov/STRR/strr-base-web#feature-app-security', { install: true }],
     // '../strr-base-web', // dev only
     '@daxiom/nuxt-core-layer-test' // extend again, this prevents the payApi plugin error
   ],
@@ -37,21 +37,11 @@ export default defineNuxtConfig({
         iso: 'en-CA',
         dir: 'ltr',
         file: 'en-CA.ts'
-      },
-      {
-        name: 'Français',
-        code: 'fr-CA',
-        iso: 'fr-CA',
-        dir: 'ltr',
-        file: 'fr-CA.ts'
       }
     ],
     strategy: 'prefix',
-    lazy: true,
-    langDir: 'locales',
     defaultLocale: 'en-CA',
-    detectBrowserLanguage: false,
-    vueI18n: './i18n.config.ts'
+    detectBrowserLanguage: false
   },
 
   runtimeConfig: {
