e5eb9edcf05d81bcccbc585d96da3da9835e0784
Jimmy Palelil <30486617+jimmypalelil@users.noreply.github.com>
2026-04-28 16:25:37 -0700

    test(noc-expiry): add unit and integration pytest suite (#1579)

--- jobs/noc_expiry/tests/unit/test_validate_test_utils.py
+++ jobs/noc_expiry/tests/unit/test_validate_test_utils.py
@@ -1,11 +1,17 @@
-from strr_api.models import User
+"""Validate the pytest-utils have been imported correctly."""
+
+import pytest
 
 
+@pytest.mark.integration
 def test_user_creation(session):
-    # 'session' triggers Postgres startup & Alembic migrations
     from strr_api.models import User
 
     user = User(lastname="Test")
     session.add(user)
     session.commit()
     assert session.query(User).count() == 1
+
+
+def test_math():
+    assert 1 + 1 == 2
