f8c4dd7a963855c6d0f923ee10e5486501801165
Jacky-Pham <admin@snoogz.ca>
2026-08-26 09:03:07 -0700

    test: apply Vite OXC setting to active config

--- strr-examiner-web/nuxt.config.ts
+++ strr-examiner-web/nuxt.config.ts
@@ -17,7 +17,6 @@ console.info('[nuxt-types-start]', JSON.stringify({
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
-  vite: { oxc: false },
 
   future: {
     compatibilityVersion: 4
@@ -94,6 +93,7 @@ export default defineNuxtConfig({
   },
 
   vite: {
+    oxc: false,
     optimizeDeps: { // optimize immediately instead of after visiting page, prevents page reload in dev when initially visiting a page with these deps
       include: ['zod', 'uuid', 'vitest']
     },
