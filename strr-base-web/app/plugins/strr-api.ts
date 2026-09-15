654c17583a427b69e4ef040ae384e844944e79c0
Jacky-Pham <admin@snoogz.ca>
2026-09-15 12:25:33 -0700

    fix: align shared api client with fetch types

--- strr-base-web/app/plugins/strr-api.ts
+++ strr-base-web/app/plugins/strr-api.ts
@@ -8,17 +8,8 @@ export default defineNuxtPlugin(() => {
   const api = $fetch.create({
     baseURL: strrApiUrl,
     onRequest ({ options }) {
-      const headers = options.headers ||= {}
-      if (Array.isArray(headers)) {
-        headers.push(['Authorization', `Bearer ${$keycloak.token}`])
-        headers.push(['Account-Id', accountStore.currentAccount.id])
-      } else if (headers instanceof Headers) {
-        headers.set('Authorization', `Bearer ${$keycloak.token}`)
-        headers.set('Account-Id', accountStore.currentAccount.id)
-      } else {
-        headers.Authorization = `Bearer ${$keycloak.token}`
-        headers['Account-Id'] = accountStore.currentAccount.id
-      }
+      options.headers.set('Authorization', `Bearer ${$keycloak.token}`)
+      options.headers.set('Account-Id', accountStore.currentAccount.id)
     },
     async onResponseError ({ response }) {
       if (response.status === 401) {
